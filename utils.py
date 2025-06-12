e38e5916164c7a3f65a6b9c1f568d2897e7590d4
Alok963-ai <alokjangde6@gmail.com>
2025-06-12 14:23:36 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,63 +1,63 @@
-import random #NIKHIL SAINI BOTS
-import time #NIKHIL SAINI BOTS
-import math #NIKHIL SAINI BOTS
-import os #NIKHIL SAINI BOTS
-from vars import CREDIT #NIKHIL SAINI BOTS
-from pyrogram.errors import FloodWait #NIKHIL SAINI BOTS
-from datetime import datetime,timedelta #NIKHIL SAINI BOTS
+import random #kaka BOTS
+import time #kaka BOTS
+import math #kaka BOTS
+import os #kaka BOTS
+from vars import CREDIT #kaka BOTS
+from pyrogram.errors import FloodWait #kaka BOTS
+from datetime import datetime,timedelta #kaka BOTS
 
 class Timer: #NIKHIL SAINI BOTS
-    def __init__(self, time_between=5): #NIKHIL SAINI BOTS
-        self.start_time = time.time() #NIKHIL SAINI BOTS
-        self.time_between = time_between #NIKHIL SAINI BOTS
+    def __init__(self, time_between=5): #KAKA BOTS
+        self.start_time = time.time() #KAKA BOTS
+        self.time_between = time_between #KAKA BOTS
 
     def can_send(self): #NIKHIL SAINI BOTS
         if time.time() > (self.start_time + self.time_between): #NIKHIL SAINI BOTS
-            self.start_time = time.time() #NIKHIL SAINI BOTS
-            return True #NIKHIL SAINI BOTS
-        return False #NIKHIL SAINI BOTS
+            self.start_time = time.time() #KAKA BOTS
+            return True #KAKA BOTS
+        return False #KAKA BOTS
 
-#lets do calculations #NIKHIL SAINI BOTS
-def hrb(value, digits= 2, delim= "", postfix=""): #NIKHIL SAINI BOTS
-    """Return a human-readable file size. #NIKHIL SAINI BOTS
+#lets do calculations #KAKA BOTS
+def hrb(value, digits= 2, delim= "", postfix=""): #KAKA BOTS
+    """Return a human-readable file size. #KAKA
     """ #NIKHIL SAINI BOTS
     if value is None: #NIKHIL SAINI BOTS
         return None #NIKHIL SAINI BOTS
     chosen_unit = "B" #NIKHIL SAINI BOTS
-    for unit in ("KB", "MB", "GB", "TB"): #NIKHIL SAINI BOTS
+    for unit in ("KB", "MB", "GB", "TB"): #KAKA BOTS
         if value > 1000: #NIKHIL SAINI BOTS
             value /= 1024 #NIKHIL SAINI BOTS
-            chosen_unit = unit #NIKHIL SAINI BOTS
+            chosen_unit = unit #KAKA BOTS
         else: #NIKHIL SAINI BOTS
-            break #NIKHIL SAINI BOTS
-    return f"{value:.{digits}f}" + delim + chosen_unit + postfix #NIKHIL SAINI BOTS
+            break #KAKA BOTS
+    return f"{value:.{digits}f}" + delim + chosen_unit + postfix #KAKA BOTS
 
-def hrt(seconds, precision = 0): #NIKHIL SAINI BOTS
-    """Return a human-readable time delta as a string. #NIKHIL SAINI BOTS
+def hrt(seconds, precision = 0): #KAKA BOTS
+    """Return a human-readable time delta as a string. #KAKA BOTS
     """ #NIKHIL SAINI BOTS
     pieces = [] #NIKHIL SAINI BOTS
-    value = timedelta(seconds=seconds) #NIKHIL SAINI BOTS
+    value = timedelta(seconds=seconds) #kaka BOTS
 
     if value.days: #NIKHIL SAINI BOTS
-        pieces.append(f"{value.days}day") #NIKHIL SAINI BOTS
+        pieces.append(f"{value.days}day") #kaka BOTS
 
-    seconds = value.seconds #NIKHIL SAINI BOTS
+    seconds = value.seconds #kaka BOTS
 
-    if seconds >= 3600: #NIKHIL SAINI BOTS
-        hours = int(seconds / 3600) #NIKHIL SAINI BOTS
-        pieces.append(f"{hours}hr") #NIKHIL SAINI BOTS
-        seconds -= hours * 3600 #NIKHIL SAINI BOTS
+    if seconds >= 3600: #kaka BOTS
+        hours = int(seconds / 3600) #kaka BOTS
+        pieces.append(f"{hours}hr") #kaka BOTS
+        seconds -= hours * 3600 #kaka BOTS
 
-    if seconds >= 60: #NIKHIL SAINI BOTS
-        minutes = int(seconds / 60) #NIKHIL SAINI BOTS
-        pieces.append(f"{minutes}min") #NIKHIL SAINI BOTS
-        seconds -= minutes * 60 #NIKHIL SAINI BOTS
+    if seconds >= 60: #kaka BOTS
+        minutes = int(seconds / 60) #KAKA BOTS
+        pieces.append(f"{minutes}min") #KAKA BOTS
+        seconds -= minutes * 60 #KAKA BOTS
 
-    if seconds > 0 or not pieces: #NIKHIL SAINI BOTS
-        pieces.append(f"{seconds}sec") #NIKHIL SAINI BOTS
+    if seconds > 0 or not pieces: #KAKA BOTS
+        pieces.append(f"{seconds}sec") #KAKA BOTS
 
-    if not precision: #NIKHIL SAINI BOTS
-        return "".join(pieces) #NIKHIL SAINI BOTS
+    if not precision: #KAKA BOTS
+        return "".join(pieces) #KAKABOTS
 
     return "".join(pieces[:precision]) #NIKHIL SAINI BOTS
 
